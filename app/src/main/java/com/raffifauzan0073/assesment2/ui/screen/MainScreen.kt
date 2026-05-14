e65100da66d82dcd85727a2703faa6f176175f9e
Raffif Auzan <raffifauzan516@gmail.com>
2026-05-15 01:04:27 +0700

    Membuat item dapat diklik

--- app/src/main/java/com/raffifauzan0073/assesment2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/raffifauzan0073/assesment2/ui/screen/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.raffifauzan0073.assesment2.ui.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +20,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -55,6 +58,7 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
+    val context = LocalContext.current
 
         if (data.isEmpty()) {
 
@@ -87,10 +91,11 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 )
 
             LazyColumn {
-
                 items(data) {
-
-                    ListItem(it)
+                    val pesan = stringResource(R.string.x_diklik, it.nama)
+                    ListItem(it) {
+                        Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
@@ -99,10 +104,10 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ListItem(transaksi: Transaksi) {
+fun ListItem(transaksi: Transaksi, onClick: () -> Unit) {
 
     Card(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp).clickable{ onClick() }
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
